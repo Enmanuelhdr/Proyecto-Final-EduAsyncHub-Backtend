87c5f0192e4d4bfbe5075ba72b0e79c19d456a1c
MSI-KATANAGF66\junio <junior.musseb@gmail.com>
2024-02-13 20:33:11 -0400

    .

--- SolutionProyectoFinal/ProyectoFinal/obj/Release/net6.0/ProyectoFinal.AssemblyInfo.cs
+++ SolutionProyectoFinal/ProyectoFinal/obj/Release/net6.0/ProyectoFinal.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b372478f0ca6f735ccbae9f2d6483e3c33d8516f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal")]
