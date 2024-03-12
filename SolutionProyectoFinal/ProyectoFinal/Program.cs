86aed62af41b6eade81a9ead384bcd2f4cc1fda0
adalberto <adalbertobanks.dev@gmail.com>
2024-03-12 13:14:59 -0400

    Revert "Chat implementado"
    This reverts commit 2d90b6d55fc38ee5cc320bd0883f487114e69d86.

--- SolutionProyectoFinal/ProyectoFinal/Program.cs
+++ SolutionProyectoFinal/ProyectoFinal/Program.cs
@@ -1,11 +1,9 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using ProyectoFinal.Configuration;
 using ProyectoFinal.Context;
-using ProyectoFinal.Services;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,8 +15,10 @@ builder.Services.AddCors(options =>
                       builder =>
                       {
                           builder.AllowAnyOrigin()
-                                 .AllowAnyHeader()
-                                 .AllowAnyMethod();
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+
+
                       });
 });
 
@@ -27,15 +27,21 @@ builder.Services.AddDbContext<EduAsyncHubContext>(options =>
     options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
+builder.Services.GetDependencyInjections();
 
-builder.Services.AddSignalR();
 
-builder.Services.AddAuthentication(config =>
-{
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(config => {
+
     config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
     config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
 }).AddJwtBearer(config =>
 {
+
     config.RequireHttpsMetadata = false;
     config.SaveToken = true;
     config.TokenValidationParameters = new TokenValidationParameters
@@ -47,6 +53,7 @@ builder.Services.AddAuthentication(config =>
         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["settings:Key"]))
     };
 });
+
 
 builder.Services.AddSwaggerGen(option =>
 {
@@ -67,8 +74,8 @@ builder.Services.AddSwaggerGen(option =>
             {
                 Reference = new OpenApiReference
                 {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
                 }
             },
             new string[]{}
@@ -78,26 +85,21 @@ builder.Services.AddSwaggerGen(option =>
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseRouting(); 
-
 app.UseCors(misReglasCors);
 
+app.UseHttpsRedirection();
+
 app.UseAuthentication();
+
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-    endpoints.MapHub<ChatHub>("/chat");
-});
+app.MapControllers();
 
 app.Run();
-
